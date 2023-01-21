115e8afd65dab0df574fc00f160a16d6e95ed274
alex <alex28042@gmail.com>
2023-01-21 19:31:05 +0100

    Admob implemented

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { BannerAd, BannerAdSize, TestIds } from "react-native-google-mobile-ads";
+
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <BannerAd
+        unitId={"ca-app-pub-9887453620475057/8458012314"}
+        size={BannerAdSize.SMART_BANNER}
+        requestOptions={{
+          requestNonPersonalizedAdsOnly: true,
+        }}
+        onAdLoaded={() => {
+          console.log("Advert loaded");
+        }}
+        onAdFailedToLoad={(error) => {
+          console.error("Advert failed to load: ", error);
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +32,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
